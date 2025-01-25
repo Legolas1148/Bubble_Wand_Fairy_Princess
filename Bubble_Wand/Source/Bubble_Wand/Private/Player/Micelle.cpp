530d5daacf694cfef5fbf4f87c5aa7bdd1c6da24
Legolas1148 <niteeshrawal1@gmail.com>
2025-01-25 18:58:30 -0500

    Added Micelle Character Script
    Implemented Character script for Micelled. Added Movement and glide logic.

---
+++ Bubble_Wand/Source/Bubble_Wand/Private/Player/Micelle.cpp
@@ -0,0 +1,151 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/Micelle.h"
+#include "Camera/CameraComponent.h"
+#include "Bubble.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Engine/World.h"
+
+// Sets default values
+AMicelle::AMicelle()
+{
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+
+    // Camera Boom
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(RootComponent);
+    CameraBoom->TargetArmLength = 400.0f; // Distance from the character model
+    CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+	//Follow Camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom);
+	FollowCamera->bUsePawnControlRotation = false;
+}
+
+// Called when the game starts or when spawned
+void AMicelle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	DefaultGravityScale = GetCharacterMovement()->GravityScale;
+}
+
+// Called every frame
+void AMicelle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bIsGliding)
+	{
+		FVector ForwardMovement = GetActorForwardVector() * 100.0f;
+		FVector RightMovement = GetActorRightVector() * 100.0f;
+		AddMovementInput(ForwardMovement, 1.0f);
+		AddMovementInput(RightMovement, 1.0f);
+	}
+}
+
+// Called to bind functionality to input
+void AMicelle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	jumpCount = 0;
+	maxJumpCount = 2;
+
+	bIsGliding = false;
+	GlideGravityScale = 0.02f;
+	DefaultGravityScale = 2.0f;
+
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMicelle::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMicelle::MoveRight);
+
+	PlayerInputComponent->BindAxis("LookUp", this, &AMicelle::LookUp);
+	PlayerInputComponent->BindAxis("Turn", this, &AMicelle::Turn);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMicelle::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AMicelle::StopJumping);
+
+	PlayerInputComponent->BindAction("Shoot", IE_Pressed, this, &AMicelle::shoot);
+}
+
+void AMicelle::MoveForward(float input)
+{
+	AddMovementInput(GetActorForwardVector(), input);
+}
+
+void AMicelle::MoveRight(float input)
+{
+	AddMovementInput(GetActorRightVector(), input);
+}
+
+void AMicelle::LookUp(float input)
+{
+	AddControllerPitchInput(input);
+}
+
+void AMicelle::Turn(float input)
+{
+	AddControllerYawInput(input);
+}
+
+void AMicelle::Jump()
+{
+	if (jumpCount < maxJumpCount)
+	{
+		Super::Jump();
+		jumpCount++;
+	}
+	else if (jumpCount == maxJumpCount && !(GetCharacterMovement()->IsMovingOnGround()))
+	{
+		StartGlide();
+	}
+}
+
+void AMicelle::StopJumping()
+{
+	Super::StopJumping();
+}
+
+void AMicelle::Landed(const FHitResult& Hit)
+{
+	Super::Landed(Hit);
+	StopGlide();
+	jumpCount = 0;
+}
+
+void AMicelle::StartGlide()
+{
+	bIsGliding = true;
+	GetCharacterMovement()->GravityScale = GlideGravityScale;
+
+	GetCharacterMovement()->MaxFlySpeed = 600.0f;
+}
+
+
+void AMicelle::StopGlide()
+{
+	if (bIsGliding)
+	{
+		bIsGliding = false;
+		GetCharacterMovement()->GravityScale = DefaultGravityScale;
+		GetCharacterMovement()->MaxFlySpeed = 0.0f;
+	}
+}
+
+void AMicelle::shoot()
+{
+	if (Bubble)
+	{
+		FVector SpawnLocation = GetActorLocation() + BubbleSpawnOffset;
+		FRotator SpawnRotation = GetActorRotation();
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			GetWorld()->SpawnActor<ABubble>(Bubble, SpawnLocation, SpawnRotation);
+		}
+	}
+}
---
+++ Bubble_Wand/Source/Bubble_Wand/Public/Bubble.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Bubble.generated.h"
+
+UCLASS()
+class BUBBLE_WAND_API ABubble : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABubble();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BluePrintReadOnly, Category = "Movement")
+	class UProjectileMovementComponent* ProjectileMovement;
+
+	UPROPERTY(VisibleAnywhere, BluePrintReadOnly, Category = "Collision")
+	class USphereComponent* CollisionComponent;
+	
+	UPROPERTY(VisibleAnywhere, BluePrintReadOnly, Category = "Visual")
+	class UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Combat")
+	float Damage = 10.0f;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+};
